72d659e71b8b156eb8e5287a3c25f09406320cc9
Filip Rajzer <rajzer.filip@gmail.com>
2021-01-07 01:48:43 +0100

    minor fix to make NodeJS stop poiting out erros in unused module

--- CMS/Lombard_00/ClientApp/src/app/sec-user/auth.service.ts
+++ CMS/Lombard_00/ClientApp/src/app/sec-user/auth.service.ts
@@ -13,20 +13,20 @@ export class AuthService {
 
   // i put it here cuz i can't get angular paths to work. will need to ask today in a few hours.
 
-  edit(nick: string, name: string, surname: string, password: string): Observable<User> {
-    return this.http.post<any>('api/user/edit',
-      { nick: nick.trim(), name: name.trim(), surname: surname.trim(), password: password.trim() })
-      .pipe(
-        rx.map((user: User) => {
-          if (user.success) {
-            user.authdata = window.btoa(user.token);
-            localStorage.setItem('currentUser', JSON.stringify(user));
-            this.currentUserSubject.next(user);
-            return user;
-          } else {
-            return null;
-          }
-        })
-      );
+  edit(nick: string, name: string, surname: string, password: string)/*: Observable<User>*/ {
+    //return this.http.post<any>('api/user/edit',
+    //  { nick: nick.trim(), name: name.trim(), surname: surname.trim(), password: password.trim() })
+    //  .pipe(
+    //    rx.map((user: User) => {
+    //      if (user.success) {
+    //        user.authdata = window.btoa(user.token);
+    //        localStorage.setItem('currentUser', JSON.stringify(user));
+    //        this.currentUserSubject.next(user);
+    //        return user;
+    //      } else {
+    //        return null;
+    //      }
+    //    })
+    //  );
   }
 }

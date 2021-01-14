2789feff5657b8c8cb1264a80dec20ea25ed4864
Filip Rajzer <rajzer.filip@gmail.com>
2021-01-14 13:29:58 +0100

    bids

--- CMS/Lombard_00/Controllers/Tranzit/TokenItem.cs
+++ CMS/Lombard_00/Controllers/Tranzit/TokenItem.cs
@@ -22,6 +22,13 @@ namespace Lombard_00.Controllers.Tranzit
             FinallizationDateTimeDouble = (new DateTime(1970, 1, 1) - FinallizationDateTime).TotalMilliseconds;
             Tags = item.Tags;
 
+            var tbids = new List<TUserItemBid>(item.Bids);
+            tbids.Remove(item.StartingBid);
+            var tbid = tbids.OrderBy(e => e.Money).FirstOrDefault();
+            if (tbid != null)
+                WinningBid = new TokenBid(tbid, context);
+            Bids = tbids.Select(e => new TokenBid(e, context));
+
             //optional - rating
             RatingAvarage = item.RatingAvarage;
             NumberOfRatings = item.NumberOfRatings;
@@ -52,8 +59,10 @@ namespace Lombard_00.Controllers.Tranzit
         public string Description { get; set; }
         public string ImageMetaData { get; set; }
         public byte[] Image { get; set; }
-        public TokenBid StartingBid { get; set; }
-        public TokenBid WinningBid { get; set; }
+        public TokenBid StartingBid { get; set; }//owner
+        public TokenBid BestBid { get; set; }//current best
+        public TokenBid WinningBid { get; set; }//winner <- after auction end
+        public IEnumerable<TokenBid> Bids { get; set; }//all bids \ owner
 
         public DateTime FinallizationDateTime { get; set; }
         public double FinallizationDateTimeDouble { get; set; }
